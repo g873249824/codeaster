85053614eda39cd98289712ff5bd8ee820d7710b
Code_Aster <code-aster at edf fr@localhost>
2013-03-14 08:10:22 +0100

    [asrest] chansard: 6 fiches
    - solde #20151: CALC_G : option CALC_K_G condition non respectee
    - solde #20115: Deux petites corrections
    - solde #20236: variable non initialis?e dans dladap.f
    - solde #20124: En NEW11.03.02, le cas-test sdll148a est NOOK sur Rocks.
    - solde #20162: DEF5 : INCO erreur dans l'option SIEF_ELGA des ?l?ments en
                    petites d?formations
    - solde #20229: Probleme utilisation DD_CC avec homogeneisation BZ

--- bibpyt/Messages/compor1.py
+++ bibpyt/Messages/compor1.py
@@ -1,8 +1,8 @@
-#@ MODIF compor1 Messages  DATE 10/05/2012   AUTEUR MACOCCO K.MACOCCO 
+#@ MODIF compor1 Messages  DATE 06/02/2013   AUTEUR CHANSARD F.CHANSARD 
 # -*- coding: iso-8859-1 -*-
 #            CONFIGURATION MANAGEMENT OF EDF VERSION
 # ======================================================================
-# COPYRIGHT (C) 1991 - 2012  EDF R&D                  WWW.CODE-ASTER.ORG
+# COPYRIGHT (C) 1991 - 2013  EDF R&D                  WWW.CODE-ASTER.ORG
 # THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY  
 # IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY  
 # THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR     
@@ -367,5 +367,9 @@ Dans la définition du materiau RUPT_DUCT les coefficients de forme de la loi CZM
 
 68 : _("""
 Erreur dans le calcul du tenseur equivalent au sens de HILL.
+"""),
+
+69 : _(u"""
+La température est obligatoire pour le comportement cristallin %(k1)s.
 """),
 }
