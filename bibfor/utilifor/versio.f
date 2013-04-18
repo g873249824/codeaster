3246fb07b7debad860091481f88a236132aa8c74
Code_Aster <code-aster at edf fr@localhost>
2013-04-18 07:56:58 +0200

    [update] to version 10.8.5

--- bibfor/utilifor/versio.f
+++ bibfor/utilifor/versio.f
@@ -1,6 +1,6 @@
       SUBROUTINE VERSIO(VERS,UTIL,NIVO,DATE,EXPLOI)
 C ======================================================================
-C MODIF UTILIFOR  DATE 11/04/2013   AUTEUR aster M.ADMINISTRATEUR
+C MODIF UTILIFOR  DATE 18/04/2013   AUTEUR aster M.ADMINISTRATEUR
 C ======================================================================
 C COPYRIGHT (C) 1991 - 2013  EDF R&D                  WWW.CODE-ASTER.ORG
 C THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
@@ -35,8 +35,8 @@ C
 C
       VERS = 10
       UTIL = 8
-      NIVO = 4
-      DATE = '11/04/2013'
+      NIVO = 5
+      DATE = '18/04/2013'
       EXPLOI = .TRUE.
 C      
       END
