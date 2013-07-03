c2395359b6d8128ad9dfaf63841deba05d243569
Mathieu Courtois <mathieu.courtois@edf.fr>
2013-07-03 11:09:41 +0200

    [#21022] support #include in fortran, add checkings of fortran/C types
    - add detection of optional features as traceback: check the subroutine
      availability instead of based on compiler name
    
    - creation of asterf_config.h/asterc_config.h
    
    - all values required to build Code_Aster must be set using define()
    
    - ASTERC_xxx is used to be limited to asterc_config.h (types for example),
      others values must be a number or a string.
    
    - _USE_INTEL_IFORT is kept for aslint that uses gfortran
      (but stay on command line)
    
    - use #define for aster_constant instead of parameter (usable in C)

--- bibfor/utilitai/trabck.F90
+++ bibfor/utilitai/trabck.F90
@@ -1,6 +1,4 @@
 subroutine trabck(cmess, iexit)
-! aslint: disable=W0104
-! Dummy argument si _USE_INTEL_IFORT n'est pas defini
 ! ----------------------------------------------------------------------
 ! IMPRIME LA REMONTEE DES APPELS
 !
@@ -8,7 +6,6 @@ subroutine trabck(cmess, iexit)
 ! IN  IEXIT  : CONDITION DE SORTIE DE LA ROUTINE DE TRACEBACK
 !              AVEC LE COMPILO INTEL <0 ON REDONNE LA MAIN
 ! ----------------------------------------------------------------------
-#ifdef _USE_INTEL_IFORT
 !
 ! COPYRIGHT (C) 1991 - 2013  EDF R&D                WWW.CODE-ASTER.ORG
 !
@@ -26,15 +23,22 @@ subroutine trabck(cmess, iexit)
 ! ALONG WITH THIS PROGRAM; IF NOT, WRITE TO EDF R&D CODE_ASTER,
 ! 1 AVENUE DU GENERAL DE GAULLE, 92141 CLAMART CEDEX, FRANCE.
 !
+! aslint: disable=W1304
+#include "asterf_config.h"
+#if _USE_INTEL_IFORT && HAVE_TRACEBACKQQ == 1
     use ifcore
 #endif
     implicit none
     character(len=*) :: cmess
     integer(kind=4) :: iexit
-#ifdef _USE_INTEL_IFORT
+!   Dummy argument si HAVE_TRACEBACKQQ n'est pas defini
+    integer :: dummy
+#if _USE_INTEL_IFORT && HAVE_TRACEBACKQQ == 1
     call TRACEBACKQQ(string=cmess, USER_EXIT_CODE=iexit)
+#elif HAVE_BACKTRACE == 1
+    call backtrace
 #else
-!     ON NE PEUT PAS APPELER U2MESS (RECURSIVITE)
+!   ON NE PEUT PAS APPELER U2MESS (RECURSIVITE)
     write(6,*) 'Traceback is not provided by the compiler'
 #endif
 !
--- waftools/med.py
+++ waftools/med.py
@@ -39,19 +39,23 @@ def configure(self):
     except Errors.ConfigurationError:
         if opts.enable_hdf5 == True:
             raise
-        self.env.append_unique('DEFINES', ['_DISABLE_HDF5'])
-        self.env.append_unique('DEFINES', ['_DISABLE_MED'])
+        self.define('_DISABLE_HDF5', 1)
+        self.define('_DISABLE_MED', 1)
+        self.undefine('HAVE_HDF5')
+        self.undefine('HAVE_MED')
     else:
-        self.env.HAVE_HDF5 = True
+        self.define('HAVE_HDF5', 1)
 
     try:
         self.check_med()
     except Errors.ConfigurationError, err:
         if opts.enable_med == True:
             raise
-        self.env.append_unique('DEFINES', ['_DISABLE_MED'])
+        self.define('_DISABLE_MED', 1)
+        self.undefine('HAVE_MED')
     else:
-        self.env.HAVE_MED = True
+        self.define('HAVE_MED', 1)
+        self.env.BUILD_MED = True
 
 ###############################################################################
 
@@ -73,7 +77,7 @@ def check_hdf5(self):
 @Configure.conf
 def check_hdf5_libs(self):
     from Options import options as opts
-    check_hdf5 = partial(self.check_cc, mandatory=True, uselib_store='MED', use='MED')
+    check_hdf5 = partial(self.check_cc, mandatory=True, uselib_store='HDF5', use='HDF5 MATH')
     if opts.embed_all or opts.embed_hdf5:
         check_lib = lambda lib: check_hdf5(stlib=lib)
     else:
@@ -82,7 +86,7 @@ def check_hdf5_libs(self):
 
 @Configure.conf
 def check_hdf5_headers(self):
-    check = partial(self.check_cc, header_name='hdf5.h', uselib_store='MED', use='MED')
+    check = partial(self.check_cc, header_name='hdf5.h', uselib_store='HDF5', use='HDF5 MATH')
     self.start_msg('Checking for header hdf5.h')
     try:
         if not check(mandatory=False):
@@ -108,7 +112,7 @@ int main(void){
 }'''
     self.start_msg('Checking hdf5 version')
     try:
-        ret = self.check_cc(fragment=fragment, use='MED', uselib_store='MED',
+        ret = self.check_cc(fragment=fragment, use='HDF5 MATH', uselib_store='HDF5',
                             mandatory=True, execute=True, define_ret=True)
     except:
         self.end_msg('no', 'YELLOW')
@@ -124,10 +128,10 @@ def check_hdf5_api(self):
     self.start_msg('Checking for API hdf5 v18')
     try:
         self.to_log('check the v18 api and set H5_NO_DEPRECATED_SYMBOLS if it fails')
-        check = partial(self.check_cc, execute=True, uselib_store='MED', use='MED')
+        check = partial(self.check_cc, execute=True, uselib_store='HDF5', use='HDF5 MATH')
         v18 = check(fragment=fragv18, mandatory=False)
         if not v18:
-            self.env.append_unique('DEFINES_MED', ['H5_NO_DEPRECATED_SYMBOLS'])
+            self.define('H5_NO_DEPRECATED_SYMBOLS', 1)
         self.to_log('try again by using H5_NO_DEPRECATED_SYMBOLS')
         check(fragment=fragv18, mandatory=True)
     except:
@@ -155,7 +159,7 @@ def check_med(self):
 def check_med_libs(self):
     from Options import options as opts
     check_med = partial(self.check_cc, features='cxx cxxprogram',
-                        mandatory=True, uselib_store='MED', use='MED')
+                        mandatory=True, uselib_store='MED', use='MED HDF5')
     if opts.embed_all or opts.embed_med:
         check_lib = lambda lib: check_med(stlib=lib)
     else:
@@ -164,7 +168,7 @@ def check_med_libs(self):
 
 @Configure.conf
 def check_med_headers(self):
-    check = partial(self.check_cc, header_name='med.h', uselib_store='MED', use='MED')
+    check = partial(self.check_cc, header_name='med.h', uselib_store='MED', use='MED HDF5')
     self.start_msg('Checking for header med.h')
     try:
         if not check(mandatory=False):
@@ -192,7 +196,7 @@ int main(void){
 }'''
     self.start_msg('Checking med version')
     try:
-        ret = self.check_cc(fragment=fragment, use='MED', uselib_store='MED',
+        ret = self.check_cc(fragment=fragment, use='MED HDF5', uselib_store='MED',
                             mandatory=True, execute=True, define_ret=True)
     except:
         self.end_msg('no', 'YELLOW')
@@ -211,17 +215,10 @@ int main(void){
     printf("%d", (int)sizeof(integer));
     return 0;
 }'''
-    self.start_msg('Checking size of med_int integers')
-    use_short = False
-    try:
-        ret = self.check_cc(fragment=fragment, use='MED', uselib_store='MED',
-                            mandatory=True, execute=True,
-                            define_ret=True)
-        if int(ret) == 4 and self.env.HAVE_64_BITS:
-            self.env.append_unique('DEFINES', ['_USE_MED_SHORT_INT'])
-            use_short = True
-    except:
-        self.end_msg('no', 'YELLOW')
-        raise
-    else:
-        self.end_msg('%s%s' % (ret, ' (-D_USE_MED_SHORT_INT)' * use_short))
+    self.code_checker('MED_INT_SIZE', self.check_cc, fragment,
+                      'Checking size of med_int integers',
+                      'unexpected value for sizeof(med_int): %s',
+                      into=(4, 8))
+    #XXX compatibility
+    if self.env['MED_INT_SIZE'] == 4 and self.is_defined('_USE_64_BITS'):
+        self.define('_USE_MED_SHORT_INT', 1)
