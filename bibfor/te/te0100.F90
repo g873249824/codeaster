fdda4b2ae877224178d7b29071a96f5b2cd561b9
Karina Macocco <macocco@phimeca.com>
2014-05-20 11:24:58 +0200

    [#22080] IMPLEX en elasticite avec une force imposee sans les cas-tests

--- bibfor/te/te0100.F90
+++ bibfor/te/te0100.F90
@@ -153,6 +153,8 @@ subroutine te0100(option, nomte)
             call u2mess('F', 'ELEMENTS5_50')
         endif
         call jevech('PCONTXR', 'E', icontp)
+!       INITIALISATION DE LA CONTRAINTE EXTRAPOLEE CONTXR=CONTMR
+        call dcopy(npg1*4, zr(icontm), 1, zr(icontp), 1)          
     endif
 !
 !
