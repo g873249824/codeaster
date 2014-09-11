38ae58e5cefe0ce0d3679c51d81da1e8adb92f71
Code_Aster <code-aster@edf.fr>
2014-09-11 07:20:52 +0200

    [#22954] crpcvg.F90 replace inline functions by #define

--- bibfor/calculel/crpcvg.F90
+++ bibfor/calculel/crpcvg.F90
@@ -56,11 +56,11 @@ subroutine crpcvg(ma1, ma2, gma1, gma2, tran,&
     character(len=24) :: conne2
 !
 !     NBNOMA(IMA) = NOMBRE DE NOEUDS DE LA MAILLE IMA
-    nbnoma(ima) = zi(ilmac1-1+ima+1) - zi(ilmac1-1+ima)
+#define nbnoma(ima)  zi(ilmac1-1+ima+1) - zi(ilmac1-1+ima)
 !
 !     NUMGLM(IMA,INO) = NUMERO GLOBAL DU NOEUD INO DE LA MAILLE IMA
-    numgl1(ima,ino) = zi(iamac1-1+zi(ilmac1+ima-1)+ino-1)
-    numgl2(ima,ino) = zi(iamac2-1+zi(ilmac2+ima-1)+ino-1)
+#define numgl1(ima,ino)  zi(iamac1-1+zi(ilmac1+ima-1)+ino-1)
+#define numgl2(ima,ino)  zi(iamac2-1+zi(ilmac2+ima-1)+ino-1)
 !
 ! DEB ------------------------------------------------------------------
     call jemarq()
