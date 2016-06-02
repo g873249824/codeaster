9c5b870c984dafc4b3d861337625386e704e0429
Mathieu Courtois <mathieu.courtois@edf.fr>
2016-06-02 14:26:18 +0200

    [#25264] use argument __only_type__ not to check syntax during build stage

--- bibpyt/Noyau/N_ETAPE.py
+++ bibpyt/Noyau/N_ETAPE.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # person_in_charge: mathieu.courtois at edf.fr
 # ======================================================================
-# COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
+# COPYRIGHT (C) 1991 - 2016  EDF R&D                  WWW.CODE-ASTER.ORG
 # THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
 # IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
 # THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
@@ -177,6 +177,7 @@ class ETAPE(N_MCCOMPO.MCCOMPO):
         if type(self.definition.sd_prod) == types.FunctionType:
             d = self.cree_dict_valeurs(self.mc_liste)
             try:
+                d['__only_type__'] = True
                 sd_prod = apply(self.definition.sd_prod, (), d)
             except EOFError:
                 raise
