3d9f4e56008dbe5bc9834bc254155c65bcdecd21
Code_Aster <code-aster@edf.fr>
2017-03-24 15:29:23 +0100

    [#26030] Add environment files for EOLE

---
+++ wafcfg/eole.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+
+"""
+Configuration for aster5
+
+. $HOME/dev/codeaster/devtools/etc/env_unstable.sh
+
+waf configure --use-config=aster5 --prefix=../install/std
+waf install -p
+"""
+
+import os
+ASTER_ROOT = os.environ['ASTER_ROOT']
+
+YAMMROOT = os.environ['ROOT_SALOME']
+
+import intel
+
+def configure(self):
+    opts = self.options
+
+    intel.configure(self)
+
+    # enable TEST_STRICT on the reference server
+    self.env.append_value('DEFINES', ['TEST_STRICT'])
+
+    self.env['ADDMEM'] = 250
+    self.env.append_value('OPT_ENV', [
+        'module load icc/2016.0.047 ifort/2016.0.047 mkl/2016.0.047'])
+    self.env.append_value('LIBPATH', [
+        '/usr/lib/atlas-base/atlas',                       # for NumPy, see issue18751
+        YAMMROOT + '/prerequisites/Python-273/lib',
+        YAMMROOT + '/prerequisites/Hdf5-1814/lib',
+        YAMMROOT + '/tools/Medfichier-320/lib',
+        YAMMROOT + '/prerequisites/Metis-40/lib',
+        YAMMROOT + '/prerequisites/Mfront_stable-TFEL202/lib',
+        YAMMROOT + '/prerequisites/Mumps4-4100_aster3/SEQ/lib',
+        YAMMROOT + '/prerequisites/Scotch-5111/lib'])
+
+    self.env.append_value('INCLUDES', [
+        YAMMROOT + '/prerequisites/Python-273/include/python2.7',
+        YAMMROOT + '/prerequisites/Hdf5-1814/include',
+        YAMMROOT + '/tools/Medfichier-320/include',
+        YAMMROOT + '/prerequisites/Metis-40/Lib',
+        YAMMROOT + '/prerequisites/Mfront_stable-TFEL202/include',
+        YAMMROOT + '/prerequisites/Scotch-5111/include'])
+
+    self.env.append_value('LIB', ('pthread', 'util'))
+    self.env.append_value('LIB_SCOTCH', ('scotcherrexit'))
+    # to fail if not found
+    opts.enable_hdf5 = True
+    opts.enable_med = True
+    opts.enable_metis = True
+    opts.enable_mumps = True
+    opts.enable_scotch = True
+    opts.enable_mfront = True
+
+    opts.enable_petsc = False
+
