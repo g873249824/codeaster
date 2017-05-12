bd87de6d76a0a214b48ddfad97ba8a425611544e
Code_Aster <code-aster@edf.fr>
2017-05-12 20:48:33 +0200

    [repo] update eole waf configuration files

--- wafcfg/eole.py
+++ wafcfg/eole.py
@@ -11,7 +11,6 @@ waf install -p
 
 import os
 ASTER_ROOT = os.environ['ASTER_ROOT']
-
 YAMMROOT = os.environ['ROOT_SALOME']
 
 import intel
@@ -19,17 +18,19 @@ import intel
 def configure(self):
     opts = self.options
 
+    self.env.append_value('CXXFLAGS', ['-D_GLIBCXX_USE_CXX11_ABI=1'])
+
     intel.configure(self)
 
     # enable TEST_STRICT on the reference server
     self.env.append_value('DEFINES', ['TEST_STRICT'])
 
-    self.env['ADDMEM'] = 250
-    self.env.append_value('OPT_ENV', [
+    self.env['ADDMEM'] = 600
+    self.env.append_value('OPT_ENV', [        
+        '. /etc/profile.d/lmod.sh',
         'module load icc/2016.0.047 ifort/2016.0.047 mkl/2016.0.047'])
+
     self.env.append_value('LIBPATH', [
-        '/usr/lib/atlas-base/atlas',                       # for NumPy, see issue18751
-        YAMMROOT + '/prerequisites/Python-273/lib',
         YAMMROOT + '/prerequisites/Hdf5-1814/lib',
         YAMMROOT + '/tools/Medfichier-321/lib',
         YAMMROOT + '/prerequisites/Metis-40/lib',
@@ -38,7 +39,6 @@ def configure(self):
         YAMMROOT + '/prerequisites/Scotch-5111/lib'])
 
     self.env.append_value('INCLUDES', [
-        YAMMROOT + '/prerequisites/Python-273/include/python2.7',
         YAMMROOT + '/prerequisites/Hdf5-1814/include',
         YAMMROOT + '/tools/Medfichier-321/include',
         YAMMROOT + '/prerequisites/Metis-40/Lib',
@@ -46,7 +46,7 @@ def configure(self):
         YAMMROOT + '/prerequisites/Scotch-5111/include'])
 
     self.env.append_value('LIB', ('pthread', 'util'))
-    self.env.append_value('LIB_SCOTCH', ('scotcherrexit'))
+#    self.env.append_value('LIB_SCOTCH', ('scotcherrexit'))
     # to fail if not found
     opts.enable_hdf5 = True
     opts.enable_med = True
