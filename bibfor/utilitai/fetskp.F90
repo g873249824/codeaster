1133f5354dcbcf333fbdb621ebe5221694e51a38
Mathieu Courtois <mathieu.courtois@edf.fr>
2017-06-14 23:40:41 +0200

    [#26604] Replace usage of 'repout' in fortran code.

--- bibfor/utilitai/fetskp.F90
+++ bibfor/utilitai/fetskp.F90
@@ -1,21 +1,23 @@
+! --------------------------------------------------------------------
+! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! This file is part of code_aster.
+!
+! code_aster is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! code_aster is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+! --------------------------------------------------------------------
+
 subroutine fetskp()
 !
-! ======================================================================
-! COPYRIGHT (C) 1991 - 2015  EDF R&D                  WWW.CODE-ASTER.ORG
-! THIS PROGRAM IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR MODIFY
-! IT UNDER THE TERMS OF THE GNU GENERAL PUBLIC LICENSE AS PUBLISHED BY
-! THE FREE SOFTWARE FOUNDATION; EITHER VERSION 2 OF THE LICENSE, OR
-! (AT YOUR OPTION) ANY LATER VERSION.
-!
-! THIS PROGRAM IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL, BUT
-! WITHOUT ANY WARRANTY; WITHOUT EVEN THE IMPLIED WARRANTY OF
-! MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. SEE THE GNU
-! GENERAL PUBLIC LICENSE FOR MORE DETAILS.
-!
-! YOU SHOULD HAVE RECEIVED A COPY OF THE GNU GENERAL PUBLIC LICENSE
-! ALONG WITH THIS PROGRAM; IF NOT, WRITE TO EDF R&D CODE_ASTER,
-!   1 AVENUE DU GENERAL DE GAULLE, 92141 CLAMART CEDEX, FRANCE.
-! ======================================================================
 !    - FONCTION REALISEE:
 !       - CREATION DU GRAPHE D'ENTREE DU PARTITIONNEUR
 !       - APPEL A METIS OU EXECUTION DE SCOTCH
@@ -224,16 +226,18 @@ subroutine fetskp()
         call lxcadr(ktmp2)
         jnom(2)='fort.'//ktmp2
         jnom(3)=ktmp
-         call gtoptk('repout', rep, iret)
-         if (iret .ne. 0) then
-             vali(1) = len(rep)
-             call utmess('F', 'EXECLOGICIEL0_24', si=vali(1))
-         endif
-         lrep = lxlgut(rep)
-         if (meth .eq. 'PMETIS  ') then
-             jnom(1)=rep(1:lrep)//'/pmetis'
-         else if (meth .eq. 'KMETIS  ') then
-             jnom(1)=rep(1:lrep)//'/kmetis'
+        if (meth .eq. 'PMETIS  ') then
+            call gtoptk('prog:pmetis', jnom(1), iret)
+            if (iret .ne. 0) then
+                vali(1) = len(rep)
+                call utmess('F', 'EXECLOGICIEL0_24', sk='pmetis', si=vali(1))
+            endif
+        else if (meth .eq. 'KMETIS  ') then
+            call gtoptk('prog:kmetis', jnom(1), iret)
+            if (iret .ne. 0) then
+                vali(1) = len(rep)
+                call utmess('F', 'EXECLOGICIEL0_24', sk='kmetis', si=vali(1))
+            endif
          endif
         call aplext(niv, 3, jnom, err)
     endif
