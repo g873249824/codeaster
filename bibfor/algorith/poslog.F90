30f2f898ebc6b735eb9f9c2bf0e0ec0f5f169217
Nicolas Pignet <nicolas.pignet@edf.fr>
2018-09-19 09:16:55 +0200

    [#27924] Supress a nonphysical check and correct the consistency of criteria between GDEF_LOG routines

--- bibfor/algorith/poslog.F90
+++ bibfor/algorith/poslog.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -20,33 +20,12 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
                   lgpg, vip, ndim, fp, g,&
                   dtde, sigm, cplan, fami, mate,&
                   instp, angmas, gn, lamb, logl,&
-                  sigp, dsidep, pk2m, pk2, codret)
-!     BUT:  POST TRAITEMENT GRANDES DEFORMATIONS 2D ET 3D LOG
-!     SUIVANT ARTICLE MIEHE APEL LAMBRECHT CMAME 2002
-!     CONFIGURATION LAGRANGIENNE
-! ----------------------------------------------------------------------
-! IN  RESI    : .TRUE. SI FULL_MECA/RAPH_MECA .FALSE. SI RIGI_MECA_TANG
-! IN  RIGI    : .TRUE. SI FULL_MECA/RIGI_MECA_TANG
-! IN  TN      : CONTRAINTES ASSOCIEES AUX DEF. LOGARITHMIQUES EN T-
-! IN  TP      : CONTRAINTES ASSOCIEES AUX DEF. LOGARITHMIQUES EN T+
-! IN  FM      : GRADIENT TRANSFORMATION EN T-
-! IN  LGPG    : DIMENSION DU VECTEUR DES VAR. INTERNES POUR 1 PT GAUSS
-! VAR VIP     : VARIABLES INTERNES EN T+
-! IN  NDIM    : DIMENSION DE L'ESPACE
-! IN  FP      : GRADIENT TRANSFORMATION EN T+
-! IN  PES     : OPERATEUR DE TRANSFORMATION TN (OU TP) EN PK2
-! IN  G       : NUMERO DU POINTS DE GAUSS
-! IN  DTDE    : OPERATEUR TANGENT ISSU DE NMCOMP (6,6)
-! IN  SIGM    : CONTRAINTES DE CAUCHY EN T-
-! IN  GN      : TERMES UTILES AU CALCUL DE TL DANS POSLOG
-! IN  FETA    : TERMES UTILES AU CALCUL DE TL DANS POSLOG
-! IN  XI      : TERMES UTILES AU CALCUL DE TL DANS POSLOG
-! IN  ME      : TERMES UTILES AU CALCUL DE TL DANS POSLOG
-! OUT SIGP    : CONTRAINTES DE CAUCHY EN T+
+                  sigp, dsidep, pk2m, pk2p, codret)
 !
-! aslint: disable=,W1504
     implicit none
+!
 #include "asterf_types.h"
+#include "asterc/r8prem.h"
 #include "asterfort/d1macp.h"
 #include "asterfort/deflg2.h"
 #include "asterfort/deflg3.h"
@@ -54,27 +33,75 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
 #include "asterfort/lctr2m.h"
 #include "asterfort/pk2sig.h"
 #include "asterfort/pmat.h"
-#include "asterfort/r8inir.h"
 #include "asterfort/symt46.h"
 #include "blas/daxpy.h"
 #include "blas/dcopy.h"
-    integer :: ndim, i, j, kl, g, lgpg, ivtn, mate, codret
-    character(len=*) :: fami
-    real(kind=8) :: dtde(6, 6), trav(6, 6), trav2(6, 6)
-    real(kind=8) :: pes(6, 6), sigm(2*ndim), sigp(2*ndim), tp2(6), tn(6)
-    real(kind=8) :: fr(3, 3), vip(lgpg)
-    real(kind=8) :: detf, dsidep(6, 6), rac2, instp, angmas(*)
-    real(kind=8) :: pk2(6), tp(6), fp(3, 3), fm(3, 3), pk2m(6)
+!
+    aster_logical, intent(in) :: resi
+    aster_logical, intent(in) :: rigi
+    aster_logical, intent(in) :: cplan
+    real(kind=8), intent(in) :: tn(6)
+    real(kind=8), intent(in) :: tp(6)
+    real(kind=8), intent(in) :: fm(3, 3)
+    real(kind=8), intent(in) :: fp(3, 3)
+    integer, intent(in) :: ndim
+    integer, intent(in) :: lgpg
+    real(kind=8), intent(out) :: vip(lgpg)
+    integer, intent(in) :: g
+    real(kind=8), intent(in) :: dtde(6,6)
+    real(kind=8), intent(in) :: sigm(2*ndim)
+    character(len=*), intent(in) :: fami
+    integer, intent(in) :: mate
+    real(kind=8), intent(in) :: instp
+    real(kind=8), intent(in) :: angmas(*)
+    real(kind=8), intent(in) :: gn(3, 3)
+    real(kind=8), intent(in) :: lamb(3)
+    real(kind=8), intent(in) :: logl(3)
+    real(kind=8), intent(out) :: sigp(2*ndim)
+    real(kind=8), intent(out) :: dsidep(6, 6)
+    real(kind=8), intent(out) :: pk2m(6)
+    real(kind=8), intent(out) :: pk2p(6)
+    integer, intent(out) :: codret
+! --------------------------------------------------------------------------------------------------
+!     BUT:  POST TRAITEMENT GRANDES DEFORMATIONS 2D ET 3D LOG
+!     SUIVANT ARTICLE MIEHE APEL LAMBRECHT CMAME 2002
+!     CONFIGURATION LAGRANGIENNE (PK2)
+! --------------------------------------------------------------------------------------------------
+! in  resi    : .true. si full_meca/raph_meca .false. si rigi_meca_tang
+! in  rigi    : .true. si full_meca/rigi_meca_tang
+! in  tn      : contraintes associees aux def. logarithmiques en t-
+! in  tp      : contraintes associees aux def. logarithmiques en t+
+! in  fm      : gradient transformation en t-
+! in  lgpg    : dimension du vecteur des var. internes pour 1 pt gauss
+! var vip     : variables internes en t+
+! in  ndim    : dimension de l'espace
+! in  fp      : gradient transformation en t+
+! in  pes     : operateur de transformation tn (ou tp) en pk2
+! in  g       : numero du points de gauss
+! in  dtde    : operateur tangent issu de nmcomp (6,6)
+! in  sigm    : contrainte de cauchy en t-
+! in  gn      : termes utiles au calcul de tl dans poslog
+! in  feta    : termes utiles au calcul de tl dans poslog
+! in  xi      : termes utiles au calcul de tl dans poslog
+! in  me      : termes utiles au calcul de tl dans poslog
+! out sigp    : contraintes de cauchy en t+
+!
+! aslint: disable=W1504
+!
+    integer :: i, j, kl, ivtn
+    real(kind=8) :: trav(6, 6), trav2(6, 6)
+    real(kind=8) :: pes(6, 6), tp2(6), fr(3, 3), detf
     real(kind=8) :: tl(3, 3, 3, 3), tls(6, 6), epse(4), d1(4, 4)
-    real(kind=8) :: gn(3, 3), feta(4), xi(3, 3), me(3, 3, 3, 3), lamb(3)
-    real(kind=8) :: logl(3)
-    aster_logical :: resi, rigi, cplan
+    real(kind=8) :: feta(4), xi(3, 3), me(3, 3, 3, 3)
+    real(kind=8), parameter :: rac2 = sqrt(2.d0)
 ! ---------------------------------------------------------------------
 !********************CONTRAINTE ET FORCES INTERIEURES******************
 !
-    rac2=sqrt(2.d0)
-    codret=0
-!
+    sigp = 0.d0
+    dsidep = 0.d0
+    pk2m = 0.d0
+    pk2p = 0.d0
+    codret = 0
 !
 !     CALCUL DES PRODUITS SYMETR. DE F PAR N
     if (resi) then
@@ -83,13 +110,13 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
         call dcopy(9, fm, 1, fr, 1)
     endif
 !
-!     DETERMINANT DE LA MATRICE F A L INSTANT T+
+!     DETERMINANT DE LA MATRICE Fr
     call lcdetf(ndim, fr, detf)
 !
 !     PERTINENCE DES GRANDEURS
-    if (detf .le. 1.d-2 .or. detf .gt. 1.d2) then
+    if (detf .le. r8prem()) then
         codret = 1
-        goto 9999
+        goto 999
     endif
 !
 ! CORRECTION POUR LES CONTRAINTES PLANES
@@ -97,23 +124,23 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
 ! CF. COMP. METHODES FOR PLASTICITY - DE SOUZA-NIETO, PERIC, OWEN p.603
 !
     if (cplan) then
-        call r8inir(4, 0.d0, epse, 1)
+        epse = 0.d0
         if (resi) then
-            call d1macp(fami, mate, instp, '+', g,&
-                        1, angmas, d1)
-            do 10 i = 1, 4
-                do 10 j = 1, 4
+            call d1macp(fami, mate, instp, '+', g, 1, angmas, d1)
+            do i = 1, 4
+                do j = 1, 4
                     epse(i)=epse(i)+d1(i,j)*tp(j)
- 10             continue
+                end do
+            end do
 !           EN ELASTICITE ISTROPE
             epse(3)=d1(1,2)*(tp(1)+tp(2))
         else
-            call d1macp(fami, mate, instp, '-', g,&
-                        1, angmas, d1)
-            do 11 i = 1, 4
-                do 11 j = 1, 4
+            call d1macp(fami, mate, instp, '-', g, 1, angmas, d1)
+            do i = 1, 4
+                do j = 1, 4
                     epse(i)=epse(i)+d1(i,j)*tn(j)
- 11             continue
+                end do
+            end do
 !           EN ELASTICITE ISTROPE
             epse(3)=d1(1,2)*(tn(1)+tn(2))
         endif
@@ -122,38 +149,32 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
 !
 ! ********************* TENSEUR DE PASSAGE DE T A PK2*******************
 !
-    call deflg2(gn, lamb, logl, pes, feta,&
-                xi, me)
-!
+    call deflg2(gn, lamb, logl, pes, feta,xi, me)
 !
 ! *********************MATRICE TANGENTE(SYMETRIQUE)*********************
     if (rigi) then
 !
 !        POUR LA RIGIDITE GEOMETRIQUE : CALCUL AVEC LES PK2
-        call r8inir(6, 0.d0, tp2, 1)
+        tp2 = 0.d0
         if (.not.resi) then
-            call pk2sig(ndim, fm, detf, pk2m, sigm,&
-                        -1)
-            do 54 kl = 4, 2*ndim
+            call pk2sig(ndim, fm, detf, pk2m, sigm, -1)
+            do kl = 4, 2*ndim
                 pk2m(kl)=pk2m(kl)*rac2
- 54         continue
+            end do
             call dcopy(6, tn, 1, tp2, 1)
         else
             call dcopy(6, tp, 1, tp2, 1)
         endif
 !
-        call deflg3(gn, feta, xi, me, tp2,&
-                    tl)
+        call deflg3(gn, feta, xi, me, tp2, tl)
 !
         call symt46(tl, tls)
 !
-        call r8inir(36, 0.d0, dsidep, 1)
         call lctr2m(6, pes, trav)
         call pmat(6, trav, dtde, trav2)
         call pmat(6, trav2, pes, dsidep)
 !
-        call daxpy(36, 1.d0, tls, 1, dsidep,&
-                   1)
+        call daxpy(36, 1.d0, tls, 1, dsidep, 1)
 !
     endif
 !
@@ -161,24 +182,22 @@ subroutine poslog(resi, rigi, tn, tp, fm,&
 !
 !        TRANSFORMATION DU TENSEUR T EN PK2
 !
-        call r8inir(6, 0.d0, pk2, 1)
-        do 51 i = 1, 6
-            do 52 j = 1, 6
-                pk2(i)=pk2(i)+tp(j)*pes(j,i)
- 52         continue
- 51     continue
+        do i = 1, 6
+            do j = 1, 6
+                pk2p(i)=pk2p(i)+tp(j)*pes(j,i)
+            end do
+        end do
 !        CALCUL DES CONTRAINTES DE CAUCHY, CONVERSION LAGRANGE -> CAUCHY
-        call pk2sig(ndim, fp, detf, pk2, sigp,&
-                    1)
+        call pk2sig(ndim, fp, detf, pk2p, sigp, 1)
 !
-!C       --------------------------------
-!C       pour gagner du temps : on stocke TP comme variable interne
-!C       --------------------------------
+!       --------------------------------
+!       pour gagner du temps : on stocke TP comme variable interne
+!       --------------------------------
         ivtn=lgpg-6+1
         call dcopy(2*ndim, tp, 1, vip(ivtn), 1)
 !
     endif
 !
-9999 continue
+999 continue
 !
 end subroutine
