827ea1c42d53ce198085fbc085268021bc6cde32
Georges Devesa <georges-cc.devesa at edf.fr@localhost>
2018-11-12 12:10:47 +0100

    [#28199] correction of time step shift in ONDE_PLANE loadings for transient DYNA_VIBRA

--- bibfor/algorith/dlnew0.F90
+++ bibfor/algorith/dlnew0.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -239,7 +239,7 @@ implicit none
     endif
     if (nondp .ne. 0) then
         call fondpl(modele, mate, numedd, neq, chondp,&
-                    nondp, vecond, veonde, vaonde, tempm,&
+                    nondp, vecond, veonde, vaonde, temps,&
                     fonde)
     endif
 !
