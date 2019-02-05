88e9dc4d85b0bce6e8078bb9e0f1ead73a7cf83a
Micka?l Abbas <mickael.abbas@edf.fr>
2019-02-05 08:53:54 +0100

    [#27607] Do not compute twice the non-mechanical strains for KIT_DDI

--- bibfor/lc/lc0120.F90
+++ bibfor/lc/lc0120.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -16,7 +16,7 @@
 ! along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
 ! --------------------------------------------------------------------
 
-subroutine lc0120(fami, kpg, ksp, ndim, imate,&
+subroutine lc0120(fami, kpg, ksp, ndim, imate, l_epsi_varc,&
                   compor, crit, instam, instap, epsm,&
                   deps, sigm, vim, option, angmas,&
                   sigp, vip, tampon, typmod, icomp,&
@@ -38,8 +38,9 @@ implicit none
     character(len=16) :: compor(*), option
     character(len=8) :: typmod(*)
     character(len=*) :: fami
+aster_logical, intent(in) :: l_epsi_varc
 !
-    call plasbe(fami, kpg, ksp, typmod, imate,&
+    call plasbe(fami, kpg, ksp, typmod, imate, l_epsi_varc,&
                 crit, epsm, deps, sigm, vim,&
                 option, tampon, sigp, vip, dsidep,&
                 icomp, nvi, codret)
