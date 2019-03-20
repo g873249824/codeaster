beb7fee7f589497dab5b6534538a710051437a53
Mathieu Courtois <mathieu.courtois@edf.fr>
2019-03-20 22:04:18 +0100

    [#28646] 'import *' only allowed at module level.

--- bibpyt/Utilitai/partition.py
+++ bibpyt/Utilitai/partition.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -101,7 +101,7 @@ class MAIL_PY:
 
  EXEMPLE D'UTILISATION
  ---------------------
- from Utilitai.Partition import *
+ from Utilitai.Partition import MAIL_PY
  import numpy as N
 
  mail=LIRE_MAILLAGE();
