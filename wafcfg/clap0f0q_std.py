03e8eaf4a815a679d6bf48ba9bc45ed889b46d4e
Code_Aster <code-aster@edf.fr>
2019-04-12 13:34:53 +0200

    [repo] update clap0f0q configuration file v13

--- wafcfg/clap0f0q_std.py
+++ wafcfg/clap0f0q_std.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -28,7 +28,8 @@ waf install -p
 
 import os
 ASTER_ROOT = os.environ['ASTER_ROOT']
-YAMMROOT = ASTER_ROOT + '/public/default'
+YAMMROOT = os.environ['ROOT_SALOME']
+
 
 import official_programs
 
