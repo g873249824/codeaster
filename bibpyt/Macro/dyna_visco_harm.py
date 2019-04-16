96f56f93c1f626090932300df84c7df90dfa756b
Etienne Cheignon <etienne.cheignon@necs.fr>
2019-04-16 23:27:01 +0200

    [#28499] fix last syntax errors

--- bibpyt/Macro/dyna_visco_harm.py
+++ bibpyt/Macro/dyna_visco_harm.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # --------------------------------------------------------------------
-# Copyright (C) 1991 - 2018 - EDF R&D - www.code-aster.org
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 # This file is part of code_aster.
 #
 # code_aster is free software: you can redistribute it and/or modify
@@ -149,7 +149,7 @@ def dyna_visco_harm(self, EXCIT, list_FREQ, modes,
 
     __lfor=PROJ_VECT_BASE(BASE=__modrs,
                           NUME_DDL_GENE=__ddlplein,
-                          VECT_ASSE=__assef,);
+                          VECT_ASSE=__assef, TYPE_VECT='FORC');
 
     DETRUIRE(CONCEPT = _F (NOM = __felem,) , )
     DETRUIRE(CONCEPT = _F (NOM = __assef,) , )
