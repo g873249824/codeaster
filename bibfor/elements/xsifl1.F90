d736d3531831951a599ed088cad534ed2618557d
Sam Cuvilliez <sam.cuvilliez@edf.fr>
2019-06-17 11:16:11 +0200

    [#25819] Correction of angle calculation in coor_cyl

--- bibfor/elements/xsifl1.F90
+++ bibfor/elements/xsifl1.F90
@@ -1,5 +1,5 @@
 ! --------------------------------------------------------------------
-! Copyright (C) 1991 - 2017 - EDF R&D - www.code-aster.org
+! Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
 ! This file is part of code_aster.
 !
 ! code_aster is free software: you can redistribute it and/or modify
@@ -188,7 +188,7 @@ subroutine xsifl1(elrefp, angl, basloc, coeff, coeff3, ddlm,&
          endif
 !       CALCUL DES COORDONNEES CYLINDRIQUES
          call coor_cyl(ndim, nnop, basloc, zr(igeom), ff,&
-                       p(1:ndim,1:ndim), invp(1:ndim,1:ndim), rg, tg, l_not_zero)
+                       p, invp, rg, tg, l_not_zero)
 !       ---------------------------------------------
 !       3) CALCUL DU DEPLACEMENT
 !       ---------------------------------------------
